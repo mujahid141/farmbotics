20e5955d61cb7a0d0c5ac10b8abe51389031f342
Mujahid Anwar <mujahidanwar141@gmail.com>
2024-10-10 11:35:02 +0500

    FYP

---
+++ farmbotics/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for farmbotics project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmbotics.settings')
+
+application = get_wsgi_application()
